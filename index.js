e8d5a0f0a2785360017cb114d95df906406c7d65
Mark Lynch <mark.christinat@gmail.com>
2019-03-13 14:17:32 -0700

    added messaging and displayed when new connection is made

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", socket => console.log("a user connectied"));
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
+  });
+});
+
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+  });
+});
+
+io.on("connection", function(socket) {
+  io.emit("chat message", "A user has connected");
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+    io.emit("chat message", "A user has Disconnected");
+  });
+});
 
 http.listen(3000, () => {
   console.log("Listening on port 3000");
