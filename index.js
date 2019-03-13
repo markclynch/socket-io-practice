3c2e50af14621b5a2b53e8d6422a9b58a76dfa42
Mark Lynch <mark.christinat@gmail.com>
2019-03-13 13:39:59 -0700

    added nodemon, socket.io and core css

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// initial commit for socket-io docs practice app.
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", socket => console.log("a user connectied"));
+
+http.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
